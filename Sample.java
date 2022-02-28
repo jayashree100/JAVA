5d73567bbb9b2a15b311b6d3218bcfc2829f50c9
JAISHRI <sjayashree529@gmail.com>
2022-02-28 11:58:02 +0530

    changed in both

--- Sample.java
+++ Sample.java
@@ -1,10 +1,13 @@
 class Sample
 {
+       int b=20;
       public static void main(String[] args)
        {
               System.out.println("Hello Developer1");
               System.out.println("Developer2");  
               System.out.println("changed");//changed by developer3
+              Sample ob = new Sample();
+             System.out.println(ob.b);
 
         }
 }
