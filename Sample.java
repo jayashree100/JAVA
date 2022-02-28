b52fef9b49757106155e09dccd8ad44fc0468ca7
jayashree100 <98389698+jayashree100@users.noreply.github.com>
2022-02-28 11:50:39 +0530

    declaring variable

--- Sample.java
+++ Sample.java
@@ -1,10 +1,13 @@
 class Sample
 {
+       int a=10;
       public static void main(String[] args)
        {
               System.out.println("Hello Developer1");
               System.out.println("Developer2");  
               System.out.println("changed");//changed by developer3
+              Sample ob=new Sample();
+              System.out.println(ob.a);
 
         }
 }
