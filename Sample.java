ef65bace66d94845377b832045b2c21099f8de77
jayashree100 <98389698+jayashree100@users.noreply.github.com>
2022-02-28 11:43:09 +0530

    changed by Developer3

--- Sample.java
+++ Sample.java
@@ -3,7 +3,8 @@ class Sample
       public static void main(String[] args)
        {
               System.out.println("Hello Developer1");
-              System.out.println("Developer2");     
+              System.out.println("Developer2");  
+              System.out.println("changed");//changed by developer3
 
         }
 }
